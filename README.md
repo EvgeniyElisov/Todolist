3640908f6283c5ea224bf2a08cf1b87a4aff285d
EvgeniyElisov <zzzzz-123@inbox.ru>
2025-12-09 23:11:00 +0300

    add project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Todolist Project (React + TypeScript + Vite)
+
+## Getting Started
+
+### 1. Install dependencies:
+
+```bash
+pnpm i
+```
+
+### 2. Start app:
+
+```bash
+pnpm dev
+```
+
+Open http://localhost:3000 with your browser to see the result.
+
---
+++ src/features/auth/ui/Login/Login.tsx
@@ -0,0 +1,129 @@
+import { selectThemeMode, setIsLoggedInAC } from "@/app/app-slice"
+import { AUTH_TOKEN } from "@/common/constants"
+import { ResultCode } from "@/common/enums"
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
+import { getTheme } from "@/common/theme"
+import { useGetCaptchaQuery, useLoginMutation } from "@/features/auth/api/authApi"
+import { type LoginInputs, loginSchema } from "@/features/auth/lib/schemas"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Box } from "@mui/material"
+import Button from "@mui/material/Button"
+import Checkbox from "@mui/material/Checkbox"
+import FormControl from "@mui/material/FormControl"
+import FormControlLabel from "@mui/material/FormControlLabel"
+import FormGroup from "@mui/material/FormGroup"
+import FormLabel from "@mui/material/FormLabel"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import { useState } from "react"
+import { Controller, type SubmitHandler, useForm } from "react-hook-form"
+import styles from "./Login.module.css"
+
+export const Login = () => {
+  
+  const themeMode = useAppSelector(selectThemeMode)
+  const [login] = useLoginMutation()
+  const dispatch = useAppDispatch()
+  const theme = getTheme(themeMode)
+  const [captchaIsRequired, setCaptchaIsRequired] = useState(false)
+  const { data: captchaUrl } = useGetCaptchaQuery(undefined, { skip: !captchaIsRequired })
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<LoginInputs>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: "", password: "", rememberMe: false, captcha: "" },
+  })
+
+  const onSubmit: SubmitHandler<LoginInputs> = (data) => {
+    login(data).then((res) => {
+      if (res.data?.resultCode === ResultCode.Success) {
+        dispatch(setIsLoggedInAC({ isLoggedIn: true }))
+        localStorage.setItem(AUTH_TOKEN, res.data.data.token)
+        reset()
+      }
+      if (res.data?.resultCode === ResultCode.CaptchaError) {
+        setCaptchaIsRequired(true)
+      }
+    })
+  }
+
+  return (
+    <Grid container justifyContent={"center"}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <FormLabel>
+            <p>
+              To login get registered
+              <a
+                style={{ color: theme.palette.primary.main, marginLeft: "5px" }}
+                href="https://social-network.samuraijs.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                here
+              </a>
+            </p>
+            <p>or use common test account credentials:</p>
+            <p>
+              <b>Email:</b> free@samuraijs.com
+            </p>
+            <p>
+              <b>Password:</b> free
+            </p>
+          </FormLabel>
+          <FormGroup>
+            <TextField label="Email" margin="normal" error={!!errors.email} {...register("email")} />
+            {errors.email && <span className={styles.errorMessage}>{errors.email.message}</span>}
+            <TextField
+              type="password"
+              label="Password"
+              margin="normal"
+              error={!!errors.email}
+              {...register("password")}
+            />
+            {errors.password && <span className={styles.errorMessage}>{errors.password.message}</span>}
+            <FormControlLabel
+              label={"Remember me"}
+              control={
+                <Controller
+                  name={"rememberMe"}
+                  control={control}
+                  render={({ field: { value, ...field } }) => <Checkbox {...field} checked={value} />}
+                />
+              }
+            />
+            {captchaUrl && (
+              <>
+                <Box
+                  component={"img"}
+                  sx={{
+                    height: 200,
+                    width: 300,
+                  }}
+                  src={captchaUrl?.url}
+                />
+                <TextField
+                  type="captcha"
+                  label="Captcha"
+                  margin="normal"
+                  error={!!errors.captcha}
+                  {...register("captcha")}
+                />
+                {errors.captcha && <span className={styles.errorMessage}>{errors.captcha.message}</span>}
+              </>
+            )}
+
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
+    </Grid>
+  )
+}
